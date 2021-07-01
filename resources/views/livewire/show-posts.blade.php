8759d8b4589803e804c7d1893626ed2dd6ac7292
Santdev404 <santdev404@gmail.com>
2021-07-01 10:18:50 -0500

    acciones livewire

--- resources/views/livewire/show-posts.blade.php
+++ resources/views/livewire/show-posts.blade.php
@@ -23,14 +23,66 @@
                 <table class="min-w-full divide-y divide-gray-200">
                     <thead class="bg-gray-50">
                     <tr>
-                        <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                        <th wire:click="order('id')" scope="col" class="cursor-pointer px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
                             ID
+
+                            {{-- Sort --}}
+                            @if ($sort === 'id')
+
+                                @if ($direction == 'asc')
+                                    <i class="fas fa-sort-alpha-up-alt float-right mt-1"></i>
+                                    
+                                @else
+                                    <i class="fas fa-sort-alpha-down-alt float-right mt-1"></i>
+                                    
+                                @endif
+                                
+                            @else
+                                
+                                <i class="fas fa-sort float-right mt-1"></i>
+                                
+                            @endif
+
                         </th>
-                        <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                        <th wire:click="order('title')" scope="col" class="cursor-pointer px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
                             Title
+
+                            {{-- Sort --}}
+                            @if ($sort === 'title')
+
+                                @if ($direction == 'asc')
+                                    <i class="fas fa-sort-alpha-up-alt float-right mt-1"></i>
+                                    
+                                @else
+                                    <i class="fas fa-sort-alpha-down-alt float-right mt-1"></i>
+                                    
+                                @endif
+                                
+                            @else
+                               
+                                <i class="fas fa-sort float-right mt-1"></i>
+                                
+                            @endif
                         </th>
-                        <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                        <th wire:click="order('content')" scope="col" class="cursor-pointer px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
                             Content
+
+                            {{-- Sort --}}
+                            @if ($sort === 'content')
+
+                                @if ($direction == 'asc')
+                                    <i class="fas fa-sort-alpha-up-alt float-right mt-1"></i>
+                                    
+                                @else
+                                    <i class="fas fa-sort-alpha-down-alt float-right mt-1"></i>
+                                    
+                                @endif
+                                
+                            @else
+                                
+                                <i class="fas fa-sort float-right mt-1"></i>
+                                
+                            @endif
                         </th>
     
                         <th scope="col" class="relative px-6 py-3">
